827fe0e418bba5068935cf6750b84b9b52d252d1
thedwilliams <thedwilliams@ccb56397-a49f-4ed5-7679-98dab609db75>
2010-06-23 09:26:12 +0000

    git-svn-id: http://pelops.googlecode.com/svn/trunk@5 ccb56397-a49f-4ed5-7679-98dab609db75

--- src/org/wyki/cassandra/pelops/KeyDeletor.java
+++ src/org/wyki/cassandra/pelops/KeyDeletor.java
@@ -2,49 +2,55 @@ package org.wyki.cassandra.pelops;
 
 import org.apache.cassandra.thrift.ColumnPath;
 import org.apache.cassandra.thrift.ConsistencyLevel;
+import org.apache.commons.lang.NotImplementedException;
 import org.wyki.cassandra.pelops.ThriftPool.Connection;
 
 /**
- * Facilitates the removal of data at a key-level. 
- * 
+ * Facilitates the removal of data at a key-level.
+ *
  * @author dominicwilliams
  *
  */
 public class KeyDeletor extends KeyspaceOperand {
 
 	private final long timestamp;
-	
+
 	/**
-	 * Delete an entire row, which is identified by its key, and all of the column families that it contains.
-	 * @param rowKey					The key of the row to delete
+	 * Delete all rows with the specified key (that is, for each column family, remove any row that has the
+	 * specified key, such that the database no longer stores any data for the key).
+	 * @param rowKey					The key of the rows to be deleted
 	 * @param cLevel					The Cassandra consistency level to be used
 	 * @throws Exception
 	 */
-	public void deleteRow(final String rowKey, final ConsistencyLevel cLevel) throws Exception {
+	public void deleteKey(final String rowKey, final ConsistencyLevel cLevel) throws Exception {
+		/*
 		IOperation operation = new IOperation() {
 			@Override
 			public Object execute(Connection conn) throws Exception {
-				
+
 				ColumnPath path = new ColumnPath();
-				conn.getAPI().remove(keyspace, rowKey, path, timestamp, cLevel);	
+				conn.getAPI().remove(keyspace, rowKey, path, timestamp, cLevel);
 				return null;
 			}
 		};
 		tryOperation(operation);
+		*/
+		throw new NotImplementedException();
 	}
-	
+
 	/**
-	 * Delete an entire column family from a row, which is identified by its key.
+	 * Delete a row with a specified key from a specified column family. The function succeeds even if
+	 * the row does not exist.
 	 * @param rowKey					The key of the row
-	 * @param columnFamily				The column family to delete
+	 * @param columnFamily				The column family from which to delete the row
 	 * @param cLevel					The Cassandra consistency level to be used
 	 * @throws Exception
 	 */
-	public void deleteColumnFamily(final String rowKey, final String columnFamily, final ConsistencyLevel cLevel) throws Exception {
+	public void deleteRow(final String rowKey, final String columnFamily, final ConsistencyLevel cLevel) throws Exception {
 		IOperation operation = new IOperation() {
 			@Override
 			public Object execute(Connection conn) throws Exception {
-				
+
 				ColumnPath path = new ColumnPath(columnFamily);
 				conn.getAPI().remove(keyspace, rowKey, path, timestamp, cLevel);
 				return null;
@@ -52,11 +58,11 @@ public class KeyDeletor extends KeyspaceOperand {
 		};
 		tryOperation(operation);
 	}
-	
+
 	protected KeyDeletor(ThriftPool thrift, String keyspace) {
 		this(thrift, keyspace, System.currentTimeMillis() * 1000);
 	}
-	
+
 	protected KeyDeletor(ThriftPool thrift, String keyspace, long timestamp) {
 		super(thrift, keyspace);
 		this.timestamp = timestamp;
