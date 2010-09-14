2831b59704d8c23c230997e37d3f5db8e9d96046
Dan Washusen <dan@reactive.org>
2010-09-14 12:47:58 +1000

    Pulled out a re-usable test helper that deals with keyspace and column family creation, etc.

--- src/test/java/org/scale7/cassandra/pelops/SelectorIntegrationTest.java
+++ src/test/java/org/scale7/cassandra/pelops/SelectorIntegrationTest.java
@@ -2,12 +2,14 @@ package org.scale7.cassandra.pelops;
 
 import org.apache.cassandra.thrift.*;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.scale7.portability.SystemProxy;
 import org.slf4j.Logger;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
@@ -22,58 +24,38 @@ import static org.scale7.cassandra.pelops.ColumnFamilyManager.*;
 public class SelectorIntegrationTest {
     private static final Logger logger = SystemProxy.getLoggerFromFactory(SelectorIntegrationTest.class);
 
-    public static final String KEYSPACE = "PelopsTesting";
     public static final String CF = "SEL_CF";
     public static final String SCF = "SEL_SCF";
 
-    private static Cluster cluster = new Cluster("localhost", 9160);
-    private static IThriftPool pool;
+    private static IntegrationTestHelper helper = new IntegrationTestHelper();
 
     @BeforeClass
-    public static void setupPool() throws Exception {
-        KeyspaceManager keyspaceManager = new KeyspaceManager(cluster);
-
-        // start from scratch
-        List<KsDef> keyspaces = keyspaceManager.getKeyspaceNames();
-        for (KsDef ksDef : keyspaces)
-        	if (ksDef.name.equals(KEYSPACE)) {
-        		keyspaceManager.dropKeyspace(KEYSPACE);
-        	}
-
-        /* The horror... the horror... as Colonel Kurtz used to say
-         * - unfortunately Pelops-Testing became an illegal keyspace name
-         * and worse than that, all the replication strategies have changed... you may even
-         * need to rebuild your schemas to map rack_unaware_strategy -> simple_strategy!
-         * ahhyeeeeeeee
-         */
-
-        KsDef keyspaceDefinition = new KsDef(KEYSPACE, KeyspaceManager.KSDEF_STRATEGY_SIMPLE, 1, new ArrayList<CfDef>());
-
-        // add a standard column family
-        keyspaceDefinition.addToCf_defs(
-                new CfDef(KEYSPACE, CF)
+    public static void setup() throws Exception {
+        helper.setup(Arrays.asList(
+                new CfDef(IntegrationTestHelper.KEYSPACE, CF)
                         .setColumn_type(CFDEF_TYPE_STANDARD)
-                        .setComparator_type(CFDEF_COMPARATOR_BYTES)
-        );
-
-        // add a super column family
-        keyspaceDefinition.addToCf_defs(
-                new CfDef(KEYSPACE, SCF)
+                        .setComparator_type(CFDEF_COMPARATOR_BYTES),
+                new CfDef(IntegrationTestHelper.KEYSPACE, SCF)
                         .setColumn_type(CFDEF_TYPE_SUPER)
                         .setComparator_type(CFDEF_COMPARATOR_BYTES)
                         .setSubcomparator_type(CFDEF_COMPARATOR_BYTES)
-        );
+        ));
+    }
 
-        keyspaceManager.addKeyspace(keyspaceDefinition);
+    @AfterClass
+    public static void teardown() {
+        helper.teardown();
+    }
 
-        pool = new DebuggingPool(cluster, KEYSPACE, new OperandPolicy());
-
+    @Before
+    public void truncate() throws Exception {
+        helper.truncate();
         prepareData();
     }
 
-    private static void prepareData() throws Exception {
+    protected void prepareData() throws Exception {
         // prep the column family data
-        Mutator mutator = pool.createMutator();
+        Mutator mutator = helper.getPool().createMutator();
         for (long i = 0; i < 100; i++) {
             mutator.writeColumns(CF, fromLong(i), createAlphabetColumns(mutator));
         }
@@ -86,8 +68,6 @@ public class SelectorIntegrationTest {
         }
 
         mutator.execute(ConsistencyLevel.ONE);
-
-
     }
 
     private static List<Column> createAlphabetColumns(Mutator mutator) {
@@ -99,15 +79,9 @@ public class SelectorIntegrationTest {
         return columns;
     }
 
-    @AfterClass
-    public static void teardownPool() {
-    	if (pool != null)
-    		pool.shutdown();
-    }
-
     @Test
     public void testGetPageOfColumnsFromRow() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { 'a', 'b', 'c', 'd', 'e' };
         List<Column> columns = selector.getPageOfColumnsFromRow(CF, fromLong(25l), (Bytes) null, false, expectedColumns.length, ConsistencyLevel.ONE);
 
@@ -116,7 +90,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfColumnsFromRowWithOffset() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { 'f', 'g', 'h', 'i', 'j' };
         List<Column> columns = selector.getPageOfColumnsFromRow(CF, fromLong(25l), fromChar('e'), false, expectedColumns.length, ConsistencyLevel.ONE);
 
@@ -125,7 +99,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfColumnsFromRowWithNoMatches() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { };
         List<Column> columns = selector.getPageOfColumnsFromRow(CF, fromLong(25l), fromChar('z'), false, expectedColumns.length, ConsistencyLevel.ONE);
 
@@ -134,7 +108,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfColumnsFromRowWithOffsetAndInsufficientMatches() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { 'x', 'y', 'z' };
         List<Column> columns = selector.getPageOfColumnsFromRow(CF, fromLong(25l), fromChar('w'), false, 1000, ConsistencyLevel.ONE);
 
@@ -143,7 +117,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfColumnsFromRowWithOffsetThatDoesNotExist() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { 'a', 'b', 'c', 'd', 'e' };
         List<Column> columns = selector.getPageOfColumnsFromRow(CF, fromLong(25l), fromChar('`'), false, expectedColumns.length, ConsistencyLevel.ONE);
 
@@ -152,7 +126,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfColumnsFromRowWithOffsetThatDoesNotExistAndInsufficientMatches() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         List<Column> columns = selector.getPageOfColumnsFromRow(CF, fromLong(25l), fromChar('`'), false, 1000, ConsistencyLevel.ONE);
 
         assertEquals("Wrong number of columns returned", 26, columns.size());
@@ -160,7 +134,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfColumnsFromRowReverse() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { 'z', 'y', 'x', 'w', 'v' };
         List<Column> columns = selector.getPageOfColumnsFromRow(CF, fromLong(25l), (Bytes) null, true, expectedColumns.length, ConsistencyLevel.ONE);
 
@@ -169,7 +143,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfColumnsFromRowReverseWithOffset() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { 'u', 't', 's', 'r', 'q' };
         List<Column> columns = selector.getPageOfColumnsFromRow(CF, fromLong(25l), fromChar('v'), true, expectedColumns.length, ConsistencyLevel.ONE);
 
@@ -185,7 +159,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfSuperColumnsFromRow() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { 'A', 'B', 'C', 'D', 'E' };
         List<SuperColumn> superColumns = selector.getPageOfSuperColumnsFromRow(SCF, fromLong(50l), (Bytes) null, false, expectedColumns.length, ConsistencyLevel.ONE);
 
@@ -194,7 +168,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfSuperColumnsFromRowWithOffset() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { 'F', 'G', 'H', 'I', 'J' };
         List<SuperColumn> superColumns = selector.getPageOfSuperColumnsFromRow(SCF, fromLong(50l), fromChar('E'), false, expectedColumns.length, ConsistencyLevel.ONE);
 
@@ -203,7 +177,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfSuperColumnsFromRowWithOffsetAndInsufficientMatches() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { 'X', 'Y', 'Z' };
         List<SuperColumn> superColumns = selector.getPageOfSuperColumnsFromRow(SCF, fromLong(50l), fromChar('W'), false, expectedColumns.length, ConsistencyLevel.ONE);
 
@@ -212,7 +186,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfSuperColumnsFromRowWithOffsetThatDoesNotExist() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { 'A', 'B', 'C', 'D', 'E' };
         List<SuperColumn> columns = selector.getPageOfSuperColumnsFromRow(SCF, fromLong(25l), fromChar('@'), false, expectedColumns.length, ConsistencyLevel.ONE);
 
@@ -221,7 +195,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfSuperColumnsFromRowReverse() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { 'Z', 'Y', 'X', 'W', 'V' };
         List<SuperColumn> superColumns = selector.getPageOfSuperColumnsFromRow(SCF, fromLong(50l), (Bytes) null, true, expectedColumns.length, ConsistencyLevel.ONE);
 
@@ -230,7 +204,7 @@ public class SelectorIntegrationTest {
 
     @Test
     public void testGetPageOfSuperColumnsFromRowReverseWithOffset() throws Exception {
-        Selector selector = pool.createSelector();
+        Selector selector = helper.getPool().createSelector();
         char[] expectedColumns = new char[] { 'U', 'T', 'S', 'R', 'Q' };
         List<SuperColumn> superColumns = selector.getPageOfSuperColumnsFromRow(SCF, fromLong(50l), fromChar('V'), true, expectedColumns.length, ConsistencyLevel.ONE);
 
