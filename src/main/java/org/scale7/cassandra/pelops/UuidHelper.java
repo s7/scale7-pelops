d370b719696a911e399dff8beff8bd35343ada2f
Dan Washusen <dan@reactive.org>
2011-01-21 15:59:12 +1100

    More work on the TimeUUID range scans (removed the methods to build a unique UUID from a date).

--- src/main/java/org/scale7/cassandra/pelops/UuidHelper.java
+++ src/main/java/org/scale7/cassandra/pelops/UuidHelper.java
@@ -1,11 +1,9 @@
 package org.scale7.cassandra.pelops;
 
-import com.eaio.uuid.UUIDGen;
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 
+import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 import java.util.UUID;
 
 public class UuidHelper {
@@ -23,64 +21,29 @@ public class UuidHelper {
 		return java.util.UUID.fromString(new com.eaio.uuid.UUID().toString());
 	}
 
-	/**
-	 * Generate a new time UUID instance from the given timestamp.  As mentioned on the {@link com.eaio.uuid.UUIDGen} javadoc
-     * even identical values of <code>millis</code> will produce different UUIDs.
-     * <p>Note: don't confuse this with {@link #timeUuidForDate(long)}!
-	 * @param millis the timestamp to use
-     * @return A new time UUID object
-	 */
-	public static java.util.UUID newTimeUuid(long millis) {
-		return java.util.UUID.fromString(
-                new com.eaio.uuid.UUID(UUIDGen.createTime(millis), UUIDGen.getClockSeqAndNode()).toString()
-        );
-	}
-
-	/**
-	 * Generate a new time UUID instance with a UTC timestamp.  Using this method to generate UUIDs ensures that
-     * you can perform time based range scans on (super) column families that use the TimeUUID comparator.  It generates
-     * a UUID with a UTC timestamp that means your machine can change adjust timezones (e.g. daylight savings) and still
-     * fetch the current data.
-     * <p><b>NOTE</b>: This requires the optional Joda-Time dependency.
-	 * @return							A new time UUID object
-	 */
-	public static java.util.UUID newTimeUuidUTC() {
-		return java.util.UUID.fromString(
-                new com.eaio.uuid.UUID(
-                        UUIDGen.createTime(
-                                DateTimeZone.UTC.convertLocalToUTC(System.currentTimeMillis(), true)
-                        ),
-                        UUIDGen.getClockSeqAndNode()
-                ).toString()
-        );
-	}
-
     /**
-     * @see #timeUuidForDate(long)
+     * @see #nonUniqueTimeUuidForDate(long)
      */
-    public static java.util.UUID timeUuidForDate(Date d) {
-        return timeUuidForDate(d.getTime());
+    public static java.util.UUID nonUniqueTimeUuidForDate(Date d) {
+        return nonUniqueTimeUuidForDate(d.getTime());
     }
 
     /**
-     * @see #timeUuidForDate(long)
+     * @see #nonUniqueTimeUuidForDate(long)
      */
-    public static java.util.UUID timeUuidForDate(DateTime d) {
-        return timeUuidForDate(d.getMillis());
+    public static java.util.UUID nonUniqueTimeUuidForDate(Calendar c) {
+        return nonUniqueTimeUuidForDate(c.getTime());
     }
 
     /**
-     * Creates a TimeUUID using the provided DateTime instance.  Additionally, creates a new instance of the provided
-     * date in UTC if it's not already.
-     * <p>This is most useful when used in conjunction with the {@link #newTimeUuidUTC()} method to create dates.
-     * @see #timeUuidForDate(long)
+     * @see #nonUniqueTimeUuidForDate(long)
      */
-    public static java.util.UUID timeUuidForDateUTC(DateTime d) {
-        return timeUuidForDate(d.toDateTime(DateTimeZone.UTC));
+    public static java.util.UUID nonUniqueTimeUuidForDate(DateTime d) {
+        return nonUniqueTimeUuidForDate(d.getMillis());
     }
 
     /**
-     * <p>This method is useful to create a non-unique TimeUUID instance from some time other than the present.
+     * <p>This method is useful to create a <b>*non-unique*</b> TimeUUID instance from some time other than the present.
      * For example, to use as the lower bound in a SlicePredicate to retrieve all columns whose TimeUUID comes
      * after time X.
      *
@@ -90,7 +53,7 @@ public class UuidHelper {
      * @param millis Gets the milliseconds of the datetime instant from the Java epoch of 1970-01-01T00:00:00Z
      * @return return
      */
-    public static java.util.UUID timeUuidForDate(long millis) {
+    public static java.util.UUID nonUniqueTimeUuidForDate(long millis) {
         long time = millis * 10000 + NUM_100NS_INTERVALS_SINCE_UUID_EPOCH;
         long timeLow = time &       0xffffffffL;
         long timeMid = time &   0xffff00000000L;
--- src/test/java/org/scale7/cassandra/pelops/UuidHelperUnitTest.java
+++ src/test/java/org/scale7/cassandra/pelops/UuidHelperUnitTest.java
@@ -1,45 +1,22 @@
 package org.scale7.cassandra.pelops;
 
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.UUID;
 
-import static org.scale7.cassandra.pelops.UuidHelper.*;
+import static org.scale7.cassandra.pelops.UuidHelper.millisFromTimeUuid;
+import static org.scale7.cassandra.pelops.UuidHelper.nonUniqueTimeUuidForDate;
 
 public class UuidHelperUnitTest {
     @Test
     public void testTimeUuidForDate() {
         long millisSource = System.currentTimeMillis();
 
-        UUID uuid = timeUuidForDate(millisSource);
+        UUID uuid = nonUniqueTimeUuidForDate(millisSource);
 
         long millisFromUuid = millisFromTimeUuid(uuid);
 
         Assert.assertEquals("Timestamp was not equal to source", millisSource, millisFromUuid);
-    }
-
-    @Test
-    public void testNewTimeUuidUTC() {
-        DateTime utcStart = new DateTime(DateTimeZone.UTC);
-        UUID uuid = newTimeUuidUTC();
-        DateTime utcEnd = new DateTime(DateTimeZone.UTC);
-
-        long millisFromUuid = millisFromTimeUuid(uuid);
-
-        Assert.assertTrue("The new UTC UUID was does not contain a UTC based millis from epoch.",
-                utcStart.getMillis() <= millisFromUuid && utcEnd.getMillis() >= millisFromUuid);
-    }
-
-    @Test
-    public void testNewTimeUuidWithMillis() {
-        final long millis = System.currentTimeMillis();
-        UUID uuid = newTimeUuid(millis);
-
-        long millisFromUuid = millisFromTimeUuid(uuid);
-
-        Assert.assertEquals(millis, millisFromUuid);
     }
 }
